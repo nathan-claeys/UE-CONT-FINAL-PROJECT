e1e4991709ed222b60da91412788bade6e4078cf
Ewwweee <gabriel.bocquet@imt-atlantique.net>
2024-12-03 16:28:37 +0100

    Route V1

--- message/api_en/src/routes/messages/index.ts
+++ message/api_en/src/routes/messages/index.ts
@@ -5,9 +5,14 @@ import * as messageControllers from "../../controllers/index";
 
 async function message(fastify: FastifyInstance) {
   fastify.get("/", messageControllers.showDatabase);
-
+  fastify.get(
+    "/show_message_user/:receiver/sender/:sender",
+    messageControllers.showMessages
+  );
   // TODO implement addList in controller
-  //   fastify.post("/send_message", messageControllers.writeMessage);
+  fastify.post("/send_message", messageControllers.writeMessage);
+
+  fastify.delete("/delete_message", messageControllers.delMessages);
 }
 
 export default message;
