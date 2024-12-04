9e91b6ccc66a155fc94df12dd3f9ac37972999f7
estelle <estelle_mgd>
2024-12-04 11:13:13 +0100

    ajout cors

--- team/team/test/plugins/support.test.ts
+++ team/team/test/plugins/support.test.ts
@@ -2,10 +2,13 @@ import { test } from 'node:test'
 import * as assert from 'node:assert'
 
 import Fastify from 'fastify'
+import cors from 'fastify'
 import Support from '../../src/plugins/support'
+
 
 test('support works standalone', async (t) => {
   const fastify = Fastify()
+  await fastify.register(cors,{})
   void fastify.register(Support)
   await fastify.ready()
 
