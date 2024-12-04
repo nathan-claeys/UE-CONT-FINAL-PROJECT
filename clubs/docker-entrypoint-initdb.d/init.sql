95d887152478ce8ab9352833d6a5804121ff64dc
Sarah <sarah.blevin@imt-atlantique.net>
2024-12-04 08:34:16 +0100

    change user id from int to varchar

--- clubs/docker-entrypoint-initdb.d/init.sql
+++ clubs/docker-entrypoint-initdb.d/init.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS clubs (
 CREATE TABLE IF NOT EXISTS club_members (
   id INT AUTO_INCREMENT PRIMARY KEY,  -- Unique identifier for the member entry
   club_id INT NOT NULL,               -- Foreign key referencing `clubs.id`
-  user_id INT NOT NULL,               -- User ID of the member
+  user_id VARCHAR(255) NOT NULL,               -- User ID of the member
   user_name VARCHAR(255) NOT NULL,    -- Name of the user
   creator TINYINT(1) DEFAULT 0,       -- Indicates if the user is the creator
   FOREIGN KEY (club_id) REFERENCES clubs(id) ON DELETE CASCADE
