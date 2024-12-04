92dbb990531b7d2c2ff07bfd37d1f569201c9a44
alicereyy <128583531+alicereyy@users.noreply.github.com>
2024-12-04 01:02:31 +0100

    add gadget controllers and routes
    also change route to get creature by name, and add route to get creature by id

--- pokechakuchon/src/routes/creaturesRoutes.ts
+++ pokechakuchon/src/routes/creaturesRoutes.ts
@@ -1,13 +1,22 @@
 import { FastifyInstance } from 'fastify';
 import * as creaturesController from '../controllers/creaturesController';
+import * as gadgetsController from '../controllers/gadgetsController';
 
 async function creaturesRoutes(fastify: FastifyInstance) {
 
   fastify.get('/creatures', creaturesController.getAllPokemon);
-  fastify.get('/creatures/:name', creaturesController.getPokemonByName);
+  fastify.get('/creatures/id/:id', creaturesController.getPokemonById);
+  fastify.get('/creatures/name/:name', creaturesController.getPokemonByName);
   fastify.post('/creatures', creaturesController.addPokemon);
   fastify.put('/creatures/:id', creaturesController.updatePokemon);
   fastify.delete('/creatures/:id', creaturesController.deletePokemon);
+
+  fastify.get('/gadgets', gadgetsController.getAllGadget);
+  fastify.get('/gadgets/id/:id', gadgetsController.getGadgetById);
+  fastify.get('/gadgets/name/:name', gadgetsController.getGadgetByName);
+  fastify.post('/gadgets', gadgetsController.addGadget);
+  fastify.put('/gadgets/:id', gadgetsController.updateGadget);
+  fastify.delete('/gadgets/:id', gadgetsController.deleteGadget);
 }
 
 export default creaturesRoutes;
