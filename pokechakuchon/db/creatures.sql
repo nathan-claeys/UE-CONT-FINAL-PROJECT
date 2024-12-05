6a7d1867c868e207b5100af40a074b7bd0f3e981
Jihene556 <hhamoudajihene@gmail.com>
2024-12-05 19:29:41 +0100

    VF

--- pokechakuchon/db/creatures.sql
+++ pokechakuchon/db/creatures.sql
@@ -2,8 +2,8 @@ CREATE DATABASE IF NOT EXISTS creatures_db;
 
 USE creatures_db;
 
--- Table gadgets
-CREATE TABLE gadgets (
+-- Table gadget
+CREATE TABLE gadget (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, -- Auto-incrementing integer ID
   name VARCHAR(255) NOT NULL, 
   target ENUM('MY_ATTACK', 'OPPONENT_ATTACK', 'TYPE_IMPACT', 'CREDIT') NOT NULL,
@@ -18,8 +18,8 @@ CREATE TABLE pokemon (
   power INT NOT NULL
 );
 
--- Example data for gadgets
-INSERT INTO gadgets (name, target, points) VALUES
+-- Example data for gadget
+INSERT INTO gadget (name, target, points) VALUES
 ('Power Booster', 'MY_ATTACK', 50),
 ('Shield Enhancer', 'OPPONENT_ATTACK', -30),
 ('Type Changer', 'TYPE_IMPACT', 20),
